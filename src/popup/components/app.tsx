02c7476a969748fb9211b75ab4b69accaede82e8
Sixstring982 <sixstring982@gmail.com>
2022-06-13 20:05:45 -0600

    Add styled header

--- src/popup/components/app.tsx
+++ src/popup/components/app.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import { mapStyles } from "../../common/util/styles";
 
 import styleMap from "./app.scss";
+import { AppHeader } from "./header/app_header";
 import { PullRequestSummaryCard } from "./pullrequestsummary/pull_request_summary_card";
 const styles = mapStyles(styleMap);
 
 export const App = () => (
-  <section className={styles("app-wrapper")}>
+  <article className={styles("app-wrapper")}>
+    <AppHeader />
     <PullRequestSummaryCard />
-  </section>
+  </article>
 );
---
+++ src/popup/components/header/app_header.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { mapStyles } from "../../../common/util/styles";
+
+import styleMap from "./app_header.scss";
+const styles = mapStyles(styleMap);
+
+export const AppHeader = () => (
+  <div className={styles("header-wrapper")}>
+    <div className={styles("logo")}>Pull Pal</div>
+    <div className={styles("nav")}>
+      <span className={"material-symbols-outlined " + styles("fab")}>
+        settings
+      </span>
+    </div>
+  </div>
+);
