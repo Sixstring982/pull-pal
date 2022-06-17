da06dd4b2514ef092980c184ea7143f4101a8759
Sixstring982 <sixstring982@gmail.com>
2022-06-17 14:46:01 -0600

    Fix inconsistent PullRequestSummary diffs
    Previously, these diffs were computed incorrectly because the service
    worker depended on a value in memory. This change moves that value to
    local storage.

--- src/common/services/localstorage/local_storage_schema.ts
+++ src/common/services/localstorage/local_storage_schema.ts
@@ -2,13 +2,14 @@ import {
   DEFAULT_PULL_REQUEST_SUMMARY,
   PullRequestSummary,
 } from "../../../api/pull_request_summary";
+import { Delta } from "../../util/delta";
 
 export interface LocalStorageSchema {
-  readonly pullRequestSummary?: PullRequestSummary;
+  readonly pullRequestSummary?: Delta<PullRequestSummary>;
   readonly githubAccessToken?: string;
 }
 
 export const DEFAULT_LOCAL_STORAGE_SCHEMA: LocalStorageSchema = {
-  pullRequestSummary: DEFAULT_PULL_REQUEST_SUMMARY,
+  pullRequestSummary: {},
   githubAccessToken: "",
 };
