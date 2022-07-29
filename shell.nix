355e02666e5a994c51a14408b156afff35a95a97
sixstring982 <sixstring982@gmail.com>
2022-07-29 17:41:04 -0600

    Add `yarn` as nix dependency
    This change adds `yarn` to `shell.nix`, in order to make `yarn`
    available for development.

--- shell.nix
+++ shell.nix
@@ -1,7 +1,10 @@
 { pkgs ? import <nixpkgs> {} }:
 let pkgs = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/nixos-22.05.tar.gz") {};
 in pkgs.mkShell {
-  packages = [ pkgs.nodejs-16_x ];
+  packages = [
+    pkgs.nodejs-16_x
+    pkgs.yarn
+ ];
 }
 
 
